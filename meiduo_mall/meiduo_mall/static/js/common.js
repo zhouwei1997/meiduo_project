ff01d469c80e8a21fc6b2d232b063b968179ce4d
zhouwei1997 <hengyu19971023@163.com>
2023-05-07 10:49:01 +0800

    配置静态js文件

--- meiduo_mall/meiduo_mall/static/js/common.js
+++ meiduo_mall/meiduo_mall/static/js/common.js
@@ -17,13 +17,13 @@ function get_query_string(name) {
 // 生成uuid
 function generateUUID() {
     let d = new Date().getTime();
-    if(window.performance && typeof window.performance.now === "function"){
+    if (window.performance && typeof window.performance.now === "function") {
         d += performance.now(); //use high-precision timer if available
     }
-    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        let r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        let r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
     return uuid;
 }
--- meiduo_mall/meiduo_mall/static/js/slide.js
+++ meiduo_mall/meiduo_mall/static/js/slide.js
@@ -1,93 +1,86 @@
-$(function(){
-	var $slides = $('.slide li');
-	var len = $slides.length;
-	var nowli = 0;
-	var prevli = 0;
-	var $prev = $('.prev');
-	var $next = $('.next');
-	var timer = null;
-	$slides.not(':first').css({'opacity':0});
+$(function () {
+    var $slides = $('.slide li');
+    var len = $slides.length;
+    var nowli = 0;
+    var prevli = 0;
+    var $prev = $('.prev');
+    var $next = $('.next');
+    var timer = null;
+    $slides.not(':first').css({'opacity': 0});
 
-	$slides.each(function(index, el) {
-		var $li = $('<li>');
-		if(index==0)
-		{
-			$li.addClass('active');
-		}
-		$li.appendTo($('.points'));
-	});
+    $slides.each(function (index, el) {
+        var $li = $('<li>');
+        if (index == 0) {
+            $li.addClass('active');
+        }
+        $li.appendTo($('.points'));
+    });
 
-	$points = $('.points li');
-	timer = setInterval(autoplay,4000);
+    $points = $('.points li');
+    timer = setInterval(autoplay, 4000);
 
-	$('.pos_center_con').mouseenter(function() {
-		clearInterval(timer);
-	});
-	
-	$('.pos_center_con').mouseleave(function() {
-		timer = setInterval(autoplay,4000);
-	});
+    $('.pos_center_con').mouseenter(function () {
+        clearInterval(timer);
+    });
 
-	function autoplay(){
-		nowli++;
-		move();
-		$points.eq(nowli).addClass('active').siblings().removeClass('active');
-	};
+    $('.pos_center_con').mouseleave(function () {
+        timer = setInterval(autoplay, 4000);
+    });
 
-	$points.click(function() {
-		nowli = $(this).index();		
-		$(this).addClass('active').siblings().removeClass('active');
-		move();
-	});
-	$prev.click(function() {	
-		nowli--;
-		move();
-		$points.eq(nowli).addClass('active').siblings().removeClass('active');
-	});	
-	$next.click(function() {		
-		nowli++;
-		move();
-		$points.eq(nowli).addClass('active').siblings().removeClass('active');
+    function autoplay() {
+        nowli++;
+        move();
+        $points.eq(nowli).addClass('active').siblings().removeClass('active');
+    }
 
-	});
+    $points.click(function () {
+        nowli = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        move();
+    });
+    $prev.click(function () {
+        nowli--;
+        move();
+        $points.eq(nowli).addClass('active').siblings().removeClass('active');
+    });
+    $next.click(function () {
+        nowli++;
+        move();
+        $points.eq(nowli).addClass('active').siblings().removeClass('active');
 
-	function move(){
-		if(nowli==prevli)
-		{
-			return;
-		}
+    });
 
-		if(nowli<0)
-		{
-			nowli=len-1;
-			prevli = 0;
-			$slides.eq(nowli).animate({'opacity':1},800);
-			$slides.eq(prevli).animate({'opacity':0},800);
-			prevli=nowli;
-			return;
-		}
+    function move() {
+        if (nowli == prevli) {
+            return;
+        }
 
-		if(nowli>len-1)
-		{
-			nowli = 0;
-			prevli = len-1;
-			$slides.eq(nowli).animate({'opacity':1},800);
-			$slides.eq(prevli).animate({'opacity':0},800);
-			prevli=nowli;
-			return;
-		}
+        if (nowli < 0) {
+            nowli = len - 1;
+            prevli = 0;
+            $slides.eq(nowli).animate({'opacity': 1}, 800);
+            $slides.eq(prevli).animate({'opacity': 0}, 800);
+            prevli = nowli;
+            return;
+        }
 
-		if(prevli<nowli)
-		{
-			$slides.eq(nowli).animate({'opacity':1},800);
-			$slides.eq(prevli).animate({'opacity':0},800);
-			prevli=nowli;			
-		}
-		else
-		{			
-			$slides.eq(nowli).animate({'opacity':1},800);
-			$slides.eq(prevli).animate({'opacity':0},800);
-			prevli=nowli;		
-		}
-	}
+        if (nowli > len - 1) {
+            nowli = 0;
+            prevli = len - 1;
+            $slides.eq(nowli).animate({'opacity': 1}, 800);
+            $slides.eq(prevli).animate({'opacity': 0}, 800);
+            prevli = nowli;
+            return;
+        }
+
+        if (prevli < nowli) {
+            $slides.eq(nowli).animate({'opacity': 1}, 800);
+            $slides.eq(prevli).animate({'opacity': 0}, 800);
+            prevli = nowli;
+        } else {
+            $slides.eq(nowli).animate({'opacity': 1}, 800);
+            $slides.eq(prevli).animate({'opacity': 0}, 800);
+            prevli = nowli;
+        }
+    }
 });
