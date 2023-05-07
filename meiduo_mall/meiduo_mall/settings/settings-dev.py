6c6583d34eb7d116e6a7c0c161553e502ffd721f
zhouwei1997 <hengyu19971023@163.com>
2023-05-07 09:44:41 +0800

    配置开发环境-配置静态文件路径

--- meiduo_mall/meiduo_mall/settings/settings-dev.py
+++ meiduo_mall/meiduo_mall/settings/settings-dev.py
@@ -116,8 +116,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# 配置静态文件位置
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
