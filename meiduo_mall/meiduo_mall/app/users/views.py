93cdf99b8c55e2ae5283bc3ef0394e76e921436c
zhouwei1997 <hengyu19971023@163.com>
2023-05-07 13:54:47 +0800

    users-视图-提供注册页面

--- meiduo_mall/meiduo_mall/app/users/views.py
+++ meiduo_mall/meiduo_mall/app/users/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+
+class RegisterView(View):
+    """用户注册"""
+
+    def get(self, request):
+        """
+        提供用户注册页面
+        :param request:
+        :return: 注册页面
+        """
+        return render(request, 'register.html')
