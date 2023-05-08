76f836c50b761f6030b5a0a3d285f4c8bf30ea09
zhouwei1997 <hengyu19971023@163.com>
2023-05-08 22:09:03 +0800

    users-注册后端逻辑-接受参数&校验参数

--- meiduo_mall/meiduo_mall/app/users/views.py
+++ meiduo_mall/meiduo_mall/app/users/views.py
@@ -1,4 +1,7 @@
 # Create your views here.
+import re
+
+from django import http
 from django.shortcuts import render
 from django.views import View
 
@@ -13,3 +16,32 @@ class RegisterView(View):
         :return: 注册页面
         """
         return render(request, 'register.html')
+
+    def post(self, request):
+        """
+        用户注册业务逻辑
+        :param request:
+        :return:
+        """
+        # 接收参数
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
+        # 校验参数
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        if not re.match(r'^[a-zA-Z0-9_]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        if not re.match(r'^[0-9a-zA-Z]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        if password != password2:
+            return http.HttpResponseForbidden('两次输入的密码不一致')
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号')
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+        # 保存注册数据
+        # 状态保持
+        # 响应结果
