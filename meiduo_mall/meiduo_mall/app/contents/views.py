af4a062240e23b80aadd5cf5db0b58c9c52728c3
zhouwei1997 <hengyu19971023@163.com>
2023-05-08 22:40:57 +0800

    首页-视图

--- meiduo_mall/meiduo_mall/app/contents/views.py
+++ meiduo_mall/meiduo_mall/app/contents/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+    """首页广告"""
+
+    def get(self, request):
+        """提供首页广告"""
+        return render(request, 'index.html')
