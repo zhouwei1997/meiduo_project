187a3ed8e38d2c75c2c61722c6a6a4cf10f07bc7
zhouwei1997 <hengyu19971023@163.com>
2023-05-11 19:52:44 +0800

    用户注册-短信验证码-容联云SDK测试

--- meiduo_mall/meiduo_mall/app/verifications/tests.py
+++ meiduo_mall/meiduo_mall/app/verifications/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
-
 # Create your tests here.
+
+
+# 容联云通讯
+from ronglian_sms_sdk import SmsSDK
+
+# 容联云
+accId = '2c94887686c00d75018709a1f74110c8'
+# token
+accToken = '3e06f7a119d94daabe19b37342a5b85a'
+# meiduo_mall的 APP_ID
+appId = '2c94887686c00d75018709a551af10d3'
+
+
+# serverIP = 'app.cloopen.com'
+# serverPort = '8883'
+# softVersion = '2013-12-26'
+
+def send_message():
+    """
+    短信测试
+    """
+    sdk = SmsSDK(accId, accToken, appId)
+    tid = '1'
+    mobile = '15027130472'
+    datas = ('2503', '5分钟')
+    resp = sdk.sendMessage(tid, mobile, datas)
+    print(resp)
+
+
+if __name__ == '__main__':
+    send_message()
