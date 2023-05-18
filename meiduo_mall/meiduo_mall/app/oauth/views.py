350f3a86d347360669fd3c6408228eef313290b8
zhouwei1997 <hengyu19971023@163.com>
2023-05-18 22:56:56 +0800

    oauth子应用-QQ登录-获取access_token和openid

--- meiduo_mall/meiduo_mall/app/oauth/views.py
+++ meiduo_mall/meiduo_mall/app/oauth/views.py
@@ -1,10 +1,14 @@
 # Create your views here.
+import logging
+
 from QQLoginTool.QQtool import OAuthQQ
 from django import http
 from django.conf import settings
 from django.views import View
 
 from meiduo_mall.utils.response_code import RETCODE
+
+logger = logging.getLogger('django')
 
 
 class QQAuthUserView(View):
@@ -15,6 +19,19 @@ class QQAuthUserView(View):
         code = request.GET.get("code")
         if not code:
             return http.HttpResponseForbidden('获取code失败')
+
+        oauth = OAuthQQ(
+            client_id=settings.QQ_CLIENT_ID,
+            client_secret=settings.QQ_CLIENT_SECRET,
+            redirect_uri=settings.QQ_REDIRECT_URI)
+        try:
+            # 获取access_token
+            access_token = oauth.get_access_token(code)
+            # 获取openid
+            openid = oauth.get_open_id(access_token)
+        except Exception as e:
+            logging.error(e)
+            return http.HttpResponseServerError('OAuth2.0认证失败')
 
 
 class QQAuthURLView(View):
