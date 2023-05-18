fd5c05c5c60f7d9512aabe1abd2b65e18fb18bec
zhouwei1997 <hengyu19971023@163.com>
2023-05-18 22:39:22 +0800

    oauth子应用-QQ登录-获取扫码页面

--- meiduo_mall/meiduo_mall/app/oauth/urls.py
+++ meiduo_mall/meiduo_mall/app/oauth/urls.py
@@ -3,3 +3,10 @@
 # @Author : 世间无事人
 # @File : urls.py 
 # @description :
+from django.conf.urls import url
+
+from oauth import views
+
+urlpatterns = [
+    url(r'^qq/login/', views.QQAuthURLView.as_view()),
+]
--- meiduo_mall/meiduo_mall/app/oauth/views.py
+++ meiduo_mall/meiduo_mall/app/oauth/views.py
@@ -1,1 +1,25 @@
 # Create your views here.
+from QQLoginTool.QQtool import OAuthQQ
+from django import http
+from django.conf import settings
+from django.views import View
+
+from meiduo_mall.utils.response_code import RETCODE
+
+
+class QQAuthURLView(View):
+    """
+    提供QQ登录页面
+    """
+
+    def get(self, request):
+        next = request.GET.get("next")
+        # 获取QQ登录页面
+        oauth = OAuthQQ(
+            client_id=settings.QQ_CLIENT_ID,
+            client_secret=settings.QQ_CLIENT_SECRET,
+            redirect_uri=settings.QQ_REDIRECT_URI,
+            state=next)
+        login_url = oauth.get_qq_url()
+        return http.JsonResponse(
+            {'code': RETCODE.OK, 'errmsg': 'OK', 'login_url': login_url})
