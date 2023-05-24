844d43ed1a22180f182123230ee94225d0a1e914
zhouwei1997 <hengyu19971023@163.com>
2023-05-24 09:57:35 +0800

    配置邮箱参数

--- meiduo_mall/meiduo_mall/settings/settings-dev.py
+++ meiduo_mall/meiduo_mall/settings/settings-dev.py
@@ -238,3 +238,15 @@ LOGIN_URL = '/login/'
 QQ_CLIENT_ID = '101518219'
 QQ_CLIENT_SECRET = '418d84ebdc7241efb79536886ae95224'
 QQ_REDIRECT_URI = 'http://www.meiduo.site:8000/oauth_callback'
+
+
+# 邮件参数
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # 导入邮件模块
+EMAIL_HOST = 'smtp.163.com'  # 发邮件主机
+EMAIL_USE_TLS = True  # 是否使用TLS安全传输协议(用于在两个通信应用程序之间提供保密性和数据完整性)
+EMAIL_USE_SSL = False  # 是否使用SSL加密，qq企业邮箱要求使用，163邮箱设置为True的时候会报ssl的错误
+EMAIL_PORT = 25  # 发邮件端口
+EMAIL_HOST_USER = 'hengyu19971023@163.com'  # 授权的邮箱
+EMAIL_HOST_PASSWORD = 'KIQRCIYAZEBDWHCR'  # 邮箱授权时获得的密码，非注册登录密码
+EMAIL_FROM = '美多商城<hengyu19971023@163.com>'  # 发件人抬头
+EMAIL_VERIFY_URL = 'http://127.0.0.1:8000/emails/verification'  # 邮箱验证连接
