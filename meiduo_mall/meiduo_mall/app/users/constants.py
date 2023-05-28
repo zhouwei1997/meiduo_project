39ff95ec33db14cb5d9e2604a46bf2d86219f4e5
zhouwei1997 <hengyu19971023@163.com>
2023-05-28 13:33:55 +0800

    user子应用中新建常量文件

---
+++ meiduo_mall/meiduo_mall/app/users/constants.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time : 2023/5/28 13:31
+# @Author : 世间无事人
+# @File : constants.py
+# @description :
+# 邮箱验证链接有效期：24小时
+VERIFY_EMAIL_TOKEN_EXPIRES = 60 * 60 * 24
+# 用户地址上限
+USER_ADDRESS_COUNTS_LIMIT = 20
