33a614086703f030b1950401afca6c198a55ca18
zhouwei1997 <hengyu19971023@163.com>
2023-05-29 21:37:18 +0800

    省市区三级联动-查询省级数据

---
+++ meiduo_mall/meiduo_mall/app/areas/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2023/5/29 20:42 
+# @Author : 世间无事人
+# @File : urls.py 
+# @description :
+from django.conf.urls import url
+
+from areas import views
+
+urlpatterns = [
+    # 省市区三级联动
+    url(r'^areas/$', views.AreaView.as_view())
+]
