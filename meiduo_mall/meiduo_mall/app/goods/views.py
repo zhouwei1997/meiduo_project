0158d9c79fadb7d8be7c8fdb17bd8992a7820928
zhouwei1997 <hengyu19971023@163.com>
2023-06-13 21:07:04 +0800

    商品列表页-排序查询

--- meiduo_mall/meiduo_mall/app/goods/views.py
+++ meiduo_mall/meiduo_mall/app/goods/views.py
@@ -25,10 +25,24 @@ class ListView(View):
             category = GoodsCategory.objects.get(id=category_id)
         except GoodsCategory.DoesNotExist:
             return http.HttpResponseForbidden('参数category_id不存在')
+        # 获取排序规则
+        sort = request.GET.get('sort', 'default')
+        # 根据sort选择排序字段
+        if sort == 'price':
+            # 按照价格由低到高排序
+            sort_field = 'price'
+        elif sort == 'hot':
+            # 按照销量由高到低排序
+            sort_field = '-sales'
+        else:
+            sort = 'default'
+            sort_field = 'create_time'
         # 查询商品分类
         categories = get_categories()
         # 查询面包屑导航 一级-> 二级-> 三级
         breadcrumb = get_breadcrumb(category)
+        # 分页和排序查询：category_id 查询SKU，一查多
+        skus = category.sku_set.filter(is_launched=True).order_by(sort_field)
         return render(
             request, 'list.html', {
                 'categories': categories, 'breadcrumb': breadcrumb})
