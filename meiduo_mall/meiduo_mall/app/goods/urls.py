78aa5702e3384fd23fc5088339a25c77c767197e
zhouwei1997 <hengyu19971023@163.com>
2023-06-13 20:06:32 +0800

    商品列表页-定义接口、路由

---
+++ meiduo_mall/meiduo_mall/app/goods/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2023/6/13 19:50 
+# @Author : 世间无事人
+# @File : urls.py 
+# @description :
+from django.conf.urls import url
+
+from goods import views
+
+urlpatterns = [
+    # 商品列表页
+    url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name='list'),
+]
--- meiduo_mall/meiduo_mall/app/goods/views.py
+++ meiduo_mall/meiduo_mall/app/goods/views.py
@@ -1,1 +1,23 @@
 # Create your views here.
+from django import http
+from django.views import View
+
+from goods.models import GoodsCategory
+
+
+class ListView(View):
+    """商品列表页"""
+
+    def get(self, request, category_id, page_num):
+        """
+        查询并渲染商品列表页
+        :param request:
+        :param category_id: 分类id
+        :param page_num: 分页，默认： 1
+        :return:
+        """
+        # 校验category_id的范围
+        try:
+            category = GoodsCategory.objects.get(id=category_id)
+        except GoodsCategory.DoesNotExist:
+            return http.HttpResponseForbidden('参数category_id不存在')
